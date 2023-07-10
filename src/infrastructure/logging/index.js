2fcb7d59438ae4d5388ac1e8da3aa4aa22382a3a
Bruno Furtado <brunofurmon@gmail.com>
2023-07-10 20:02:02 -0300

    chore: Simpler logger

--- src/infrastructure/logging/index.js
+++ src/infrastructure/logging/index.js
@@ -3,16 +3,16 @@ const winston = require('winston');
 const init = () => {
   const logger = winston.createLogger({
     level: (process.env.LOG_LEVEL || 'debug').toLowerCase(),
-    format: winston.format.json(),
-    defaultMeta: { service: 'cart-api' },
-  });
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
 
-  // I'll be using Console only for the purpose of this API
-  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  //
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
+    // I'll be using Console only for the purpose of this API
+    // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+    transports: new winston.transports.Console(),
+  });
 
   return logger;
 };
