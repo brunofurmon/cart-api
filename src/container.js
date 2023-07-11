5abc17d95b67274ca188a0744127cbafd109f43c
Bruno Furtado <brunofurmon@gmail.com>
2023-07-11 03:55:44 -0300

    fix: DB container

--- src/container.js
+++ src/container.js
@@ -3,7 +3,7 @@ const awilix = require('awilix');
 const cartServiceBuilder = require('./domain/cart/service');
 const httpServerBuilder = require('./presentation/http/server');
 const loggerBuilder = require('./infrastructure/logging');
-const dbBuilder = require('./infrastructure/logging');
+const dbBuilder = require('./infrastructure/database/inMemory');
 const cartRepositoryBuilder = require('./infrastructure/repositories/cart');
 
 const container = awilix.createContainer();
@@ -12,8 +12,8 @@ container.register({
   cartService: awilix.asFunction(cartServiceBuilder),
   httpServer: awilix.asFunction(httpServerBuilder),
   logger: awilix.asFunction(loggerBuilder),
-  db: awilix.asFunction(dbBuilder),
   cartRepository: awilix.asFunction(cartRepositoryBuilder),
+  db: awilix.asFunction(dbBuilder),
 });
 
 module.exports = container;
